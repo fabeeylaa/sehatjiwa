3e6596c4f60c2d0a3423a436cea3935325302536
DdulBedull <qdaffaabdullah@gmail.com>
2026-08-24 11:35:25 +0700

    setup scaffold frontend (React+Vite+Tailwind) dan backend (Express+PostgreSQL)

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import pool from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('SehatJiwa API is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
