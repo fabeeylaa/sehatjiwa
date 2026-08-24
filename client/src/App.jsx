e4ca1190e64621aa0057e611360117d38ba0c475
Handykadwi <handykadwii@gmail.com>
2026-08-24 17:50:51 +0700

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,8 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-blue-600 text-center mt-10">
-      SehatJiwa Frontend Ready
+    <h1 className="text-3xl font-bold text-green-600 text-center mt-10">
+      SehatJiwa
     </h1>
   )
 }
-
 export default App
