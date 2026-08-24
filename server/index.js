f77c0e446107b92aa7ca29a7cb19759889c0f208
DdulBedull <qdaffaabdullah@gmail.com>
2026-08-25 06:59:39 +0700

    add auth register & login

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import pool from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('SehatJiwa API is running');
